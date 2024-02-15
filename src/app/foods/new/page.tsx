d2680f191c1471d92a00edd528cd47dcef356137
Jon Mico <jonmico@gmail.com>
2024-02-15 17:36:54 -0500

    Add some styling.

--- src/app/foods/new/page.tsx
+++ src/app/foods/new/page.tsx
@@ -1,37 +1,42 @@
+import styles from './page.module.css';
+
 export default function NewFoodPage() {
   async function createFood(formData: FormData) {
     'use server';
   }
   return (
-    <form action={createFood}>
-      <div>
+    <form className={styles.createFoodForm} action={createFood}>
+      <div className={styles.formField}>
         <label htmlFor='brand'>Brand</label>
         <input type='text' name={'brand'} id={'brand'} />
       </div>
-      <div>
+      <div className={styles.formField}>
         <label htmlFor='name'>Name</label>
         <input type='text' name={'name'} id={'brand'} />
       </div>
-      <div>
+      <div className={styles.formField}>
         <label htmlFor='servingSize'>Serving Size</label>
         <input type='text' name={'servingSize'} id={'servingSize'} />
       </div>
-      <div>
+      <div className={styles.formField}>
         <label htmlFor='calories'>Calories</label>
         <input type='number' name='calories' id='calories' />
       </div>
-      <div>
+      <div className={styles.formField}>
         <label htmlFor='fat'>Fat</label>
         <input type='number' name='fat' id='fat' />
       </div>
-      <div>
+      <div className={styles.formField}>
         <label htmlFor='carbs'>Carbs</label>
         <input type='number' name='carbs' id='carbs' />
       </div>
-      <div>
+      <div className={styles.formField}>
         <label htmlFor='protein'>Protein</label>
         <input type='number' name='protein' id='protein' />
       </div>
+      <button type={'submit'} className={styles.submitButton}>
+        Create Food!
+      </button>
     </form>
   );
 }
